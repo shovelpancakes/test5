f4c013b01bc5ad58824f3e70bc634453c8ddff50
edgeofcry <kerkru804@gmail.com>
2026-05-08 13:57:11 +0300

    Добавлена переменная noyes

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
-        System.out.println(" Изменение 2");
+        System.out.println(" Изменение 3");
         for (int i = 1; i <= 5; i++) {
             System.out.println("Итерация №" + i);
+        }
+        boolean noyes = true;
+        if (noyes) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
         }
     }
 }
