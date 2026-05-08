6ec467f5a85f536e4427090ce37ba554a5acfdc0
edgeofcry <kerkru804@gmail.com>
2026-05-08 13:58:41 +0300

    Добавлена переменная yesno

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
-        System.out.println(" Изменение 3");
+        System.out.println(" Изменение 4");
         for (int i = 1; i <= 5; i++) {
             System.out.println("Итерация №" + i);
         }
@@ -10,6 +10,12 @@ class Main {
             System.out.println("yes");
         } else {
             System.out.println("no");
+        }
+        boolean yesno = true;
+        if (yesno) {
+            System.out.println("no");
+        } else {
+            System.out.println("yes");
         }
     }
 }
