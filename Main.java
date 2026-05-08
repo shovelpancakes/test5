adc1d352587c4c6184276aacb8d0d7c838572d76
edgeofcry <kerkru804@gmail.com>
2026-05-08 13:53:04 +0300

    Добавлен цикл for на 5 итераций

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
-        System.out.println(" Изменение 1");
+        System.out.println(" Изменение 2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Итерация №" + i);
+        }
     }
 }
